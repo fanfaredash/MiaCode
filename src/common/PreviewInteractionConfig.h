bd025c8449dc2eea6ccfc9b8c5a0a46abda99c1c
fanfaredash <fpdqwq@yeah.net>
2026-03-26 22:34:37 +0900

    Slow repeated preview seek stepping

--- src/common/PreviewInteractionConfig.h
+++ src/common/PreviewInteractionConfig.h
@@ -5,5 +5,6 @@ namespace miacode::preview_interaction {
 inline constexpr double kSeekInitialStepSeconds = 0.016;
 inline constexpr double kSeekMaxStepSeconds = 0.250;
 inline constexpr double kSeekLinearAccelerationSecondsPerMs = 0.00024;
+inline constexpr double kSeekHeldRepeatSpeedScale = 0.5;
 
 }  // namespace miacode::preview_interaction
