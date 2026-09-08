d3c50201bd1e69b01e3b84903e3ee710e73733e8
fanfaredash <fpdqwq@yeah.net>
2026-09-08 11:14:08 +0800

    fix(audio): use four master mixer threads by default

--- src/audio/BassPreviewMasterMixerPolicy.h
+++ src/audio/BassPreviewMasterMixerPolicy.h
@@ -6,7 +6,7 @@
 namespace miacode::preview_audio::bass {
 
 inline constexpr double kDefaultMasterMixerBufferMs = 30.0;
-inline constexpr int kDefaultMasterMixerThreadCount = 1;
+inline constexpr int kDefaultMasterMixerThreadCount = 4;
 inline constexpr double kMaxMasterMixerBufferMs = 500.0;
 inline constexpr int kMaxMasterMixerThreadCount = 16;
 
